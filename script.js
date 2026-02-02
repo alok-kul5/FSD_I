cc982976d45e4d4d06c9b79b961e3ae1b30d1155
Alok Kulkarni <alokkulkarni62@gmail.com>
2026-02-02 10:31:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// smooth scrolventListener('click', function (e) {
+    e.preventl
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEDefault();
+    dont.cumequerySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
