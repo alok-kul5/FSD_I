3b9cc801821feb3cdf45cd437d2ac765e7531b3d
Alok Kulkarni <alokkulkarni62@gmail.com>
2026-02-09 09:49:34 +0530

    clean

--- script.js
+++
@@ -1,9 +0,0 @@
-// smooth scrolventListener('click', function (e) {
-    e.preventl
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEDefault();
-    dont.cumequerySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
